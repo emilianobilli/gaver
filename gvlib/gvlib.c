3aa94206c0b773f7c22c6268531c3e03cfc22fec
Nicolas Pajoni <rondal@gmail.com>
2015-06-24 20:21:46 +0000

    gvlib

--- gvlib/gvlib.c
+++ gvlib/gvlib.c
@@ -8,5 +8,113 @@ int getdatasocket(socket_t *sd)
 int getgvsocketunix(char *su_path)
 {
     strcpy(su_path, "/var/gaver.sock");
+    return 0;
 }
 
+int gv_socket(gv_socket_t *sdgv)
+{
+    int sd;
+    struct sockaddr_un addr;
+    
+    /* Inicializo la estructura del socket con el valor cero */
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    /* Asigno valores de tipo y path al socket unix */
+    addr.sun_family = AF_UNIX;
+    if (getgvsocketunix(addr.sun_path) != 0 )
+	return -1;
+
+    /* Creo el socket de control para conectarme a Gaver  */
+    if (sd = socket(PF_UNIX, SOCK_STREAM, 0) == -1)
+	return -1;
+
+    /* Me conecto al socket */
+    if (connect (sd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1)
+	return -1;
+
+    sdgv->so_ctrl = sd;
+    
+    /* Creo el socket del server */
+    if (sd = socket(PF_UNIX, SOCK_STREAM, 0) == -1)
+	return -1;
+
+    /* Genero un path aleatorio para el socket unix */
+    sprintf (addr.sun_path, "%ld-%d-%d.sock", time(), getpid(), rand()%100);
+
+    /* Realizo un bind */
+    if (bind(sd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1)
+	return -1;
+
+    /* Pongo el socket en modo listen  */
+    if (listen(sd, 5) == -1)
+	return -1;
+    
+    /* Copio el path del socket en la estructura del socket gaver */
+    sdgv->so_data = sd;
+    memcpy(&(sdgv->so_addr), &addr, sizeof(struct sockaddr_un));
+
+    return 0;
+}
+
+int gv_connect(gv_socker_t *sd, struct sockaddr_in* addr, socklen_t len)
+{
+    int ret, bytes_transfd, sd_new;
+    char *ptr;
+    /* Creo estructura para mensaje de tipo connect */
+    gv_msgapi_t cntmsg, repmsg;
+
+    cntmsg.type = GV_CONNECT_API;
+    cntmsg.un.connect.ip = addr->sin_addr.s_addr;
+    cntmsg.un.connect.port = GV_PORT;
+    cntmsg.un.connect.vport = addr->sin_port;
+    cntmsg.un.connect.path = sd->so_addr;
+
+    /* Envio mensaje connect por so_ctrl de gaver */
+    ptr = &cntmsg;
+    bytes_transfd = 0;
+    while(bytes_transfd < GV_MSGSIZE){
+	ret = send(sd->so_ctrl, &ptr[bytes_transfd], GV_MSGSIZE - bytes_transfd, 0);
+	if (ret == -1)
+	    return -1;
+	bytes_transfd += ret;
+    }
+
+    /* Leo mensaje de reply */
+    ptr = &repmsg;
+    bytes_transfd = 0;
+    while(bytes_transfd < GV_MSGSIZE){
+	ret = recv(sd->so_ctrl, &ptr[bytes_transfd], GV_MSGSIZE - bytes_transfd, 0);
+	if (ret == -1)
+	    return -1;
+	bytes_transfd += ret;
+    }
+    /* Verifico que el mensaje sea de tipo reply y el codigo de respuesta */
+    if (repmsg.type != GV_REPLY_API)
+	return -1;
+    if (repmsg.un.reply.code != GV_REP_CODE_OK)
+	return -1;
+    /* Acepto conexion de datos de Gaver con su nuevo socket descriptor */
+    if ((sd_new = accept(sd->so_data, NULL, NULL )) == -1)
+	return -1
+    sd->so_data = sd_new;
+    return 0;
+}
+
+int gv_bind(gv_socker_t *sd, struct sockaddr_in* addr, socklen_t len)
+{
+
+}
+
+int gv_listen(gv_socker_t *sd, int backlog)
+{
+
+}
+
+int gv_accept(gv_socker_t *sd, struct sockaddr_in* addr, socklen_t *len)
+{
+
+}
+
+int gv_close (gv_socker_t *sd)
+{
+
+}
--- gvlib/gvlib.h
+++ gvlib/gvlib.h
@@ -4,6 +4,21 @@
 #include <unistd.h>
 #include <socket.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define GV_CONNECT_API 0
+#define GV_REPLY_API 1
+#define GV_BIND_API 2
+#define GV_LISTEN_API 3
+#define GV_ACCEPT_API 4
+#define GV_CLOSE_API 5
+
+#define GV_PORT 55000
+#define GV_MSGSIZE 128
+
+#define GV_REP_CODE_OK 0
+#define GV_REP_CODE_FAIL 1
 
 typedef struct {
     int so_ctrl;
@@ -12,20 +27,70 @@ typedef struct {
 } gv_socket_t;
 
 
+typedef struct {
+    u_int8_t type;
+    union {
+	gv_cntapi_t connect;
+	gv_rplapi_t reply;
+	gv_bindapi_t bind;
+	gv_ltnapi_t listen;
+	gv_acptapi_t accept;
+	gv_clsapi_t close;
+    } un;
+} gv_msgapi_t;
+
+/* Mensaje de tipo Connect para la API */
+typedef struct {
+    u_int32_t ip;
+    u_int16_t port;
+    u_int16_t vport;
+    u_int8_t path[108];
+    u_int8_t res[11];
+} gv_cntapi_t;
+
+/* Mensaje de tipo reply para la API */
+typedef struct {
+    u_int8_t code;
+    u_int32_t ip;
+    u_int16_t port;
+    u_int16_t vport;
+    u_int8_t msg[118];
+} gv_rplapi_t;
+
+/* Mensaje de tipo bind para la API */
+typedef struct {
+    u_int32_t ip;
+    u_int16_t port;
+    u_int16_t vport;
+    u_int8_t res[119];
+} gv_bindapi_t;
+
+/* Mensaje de tipo listen para la API */
+typedef struct {
+    u_int8_t backlog;
+    u_int8_t res[126];
+} gv_ltnapi_t;
+
+/* Mensaje de tipo accept para la API */
+typedef struct {
+    u_int8_t path[108];
+    u_int8_t res[19];
+} gv_acptapi_t;
+
+/* Mensaje de tipo close para la API */
+typedef struct {
+    u_int8_t res[127];
+} gv_clsapi_t;
+
+
+
 int getdatasocket(socket_t *sd);
 int getgvsocketunix(char *su_path);
-
+int gv_socket(gv_socket_t *sd);
+int gv_connect(gv_socker_t *sd, struct sockaddr_in* addr, socklen_t len);
+int gv_bind(gv_socker_t *sd, struct sockaddr_in* addr, socklen_t len);
+int gv_listen(gv_socker_t *sd, int backlog);
+int gv_accept(gv_socker_t *sd, struct sockaddr_in* addr, socklen_t *len);
+int gv_close (gv_socker_t *sd);
 
 #endif /* gv_lib.h */
-
-int getdatasocket(socket_t *sd)
-{
-    return sd->so_data;
-}
-
-
-int getgvsocketunix(char *su_path)
-{
-    strcpy(su_path, "/var/gaver.sock");
-}
-
