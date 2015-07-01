0acdc5f7f290d81b989a717f4ad687e609dc09ea
emilianob <emiliano.billi@gmail.com>
2015-06-30 21:38:55 -0300

    panic

--- gv_input.c
+++
@@ -1,9 +0,0 @@
-
-
-
-#include "mbuff.h"
-
-int udp_input(int ifudp, struct msg_queue *msg)
-{
-    struct mbuff *mb;
-}
---
+++ input.c
@@ -0,0 +1,17 @@
+#include "glo.h"
+#include "input_var.h"
+#include "heap.h"
+#include "itc.h"
+#include "mbuff_queue.h"
+#include "mbuff.h"
+
+
+
+
+
+void *input (void *arg)
+{
+
+
+
+}
