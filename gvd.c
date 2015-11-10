39538afb083e73de9df8898f5195c582fdeeaaf8
emilianob <emiliano.billi@gmail.com>
2015-11-10 20:33:35 -0300

    licence

--- gvd.c
+++ gvd.c
@@ -1,3 +1,19 @@
+/*
+    This file is part of GaVer
+
+    GaVer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
@@ -154,6 +170,10 @@ int main (int argc, char *argv[])
 	exit(EXIT_FAILURE);
     }    
     
+    /* 
+     * Finaly dispach the kernel
+     */
+
     while (1)
 	sleep(5);
 
